f03577e16bcd6bb138fe652b6328f4df52f53b3a
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-01-28 15:03:37 -0600

    Deploy de aplicacion web php en Azure App Service

--- index.php
+++ index.php
@@ -1,48 +1,54 @@
-/ Hacemos un arreglo con datos de ejemplo
-$data = [
-    'title' => 'Untitled-1',
-    'created' => '2015-01-01',
-    'modified' => '2015-01-01',
-    'id' => '1',
-    'name' => 'Practica 1',
-    ]
-
-echo "
-    <!-- Make a Table for the Data -->
-        <table class='table table-striped table-bordered table-hover'>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Value</th>
-            </tr>
-        </thead>
-        <tbody>
-";
-
-// Para cada elemento del arreglo colocamos una fila
-foreach($data as $key => $value){
-    echo "
-            <tr>
-                <td>$key</td>
-                <td>$value</td>
-            </tr>
-        ";
-}
-echo "
-        </tbody>
-    </table>
-";
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Chritsmas</title>
+    <link rel="icon" type="image/x-icon" href="css/favicon/favicon.png">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Welcome to Tokio Japon</h1>
-    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.jrailpass.com%2Fblog%2Fes%2Flugares-y-tiendas-manga-anime-japon&psig=AOvVaw3uGl7UeFbuji0YmEQKbj7z&ust=1643488621760000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJCMn6-m1fUCFQAAAAAdAAAAABAD" alt="japon">
+    <div class="header">
+        <h1>La magia de la Navidad</h1>
+    </div>
+    <div class="container">
+       <div class="blog">
+            <div class="subtitle">
+                <h2>Deseos de Navidad</h2><br>        
+                <p><span>¿Qué es la Navidad?</span> Es la ternura del pasado,
+                el valor del presente y la esperanza del futuro. Es el Deseos
+                sincero de que cada taza se puede llenar con bendiciones ricas
+                y eternas, y de que cada camino nos lleva a la paz.</p>
+            </div>
+            <div class="coments">
+                <h3>Nombre</h3>
+                <span>
+                    COMENTARIO: Lorem ipsum dolor sit amet, 
+                    consectetur adipisicing elit. Eveniet, 
+                    ipsam? Assumenda reprehenderit sint porro dolorum 
+                    repudiandae et ut libero dolor officia natus quod 
+                    nesciunt obcaecati, impedit neque illo maxime id.
+                </span>
+            </div>
+       </div>
+        <div class="sidebar-right">
+            <img src="css/img/chritsmats1.png" alt="Chritsmas-right">
+        </div>
+        <div class="sidebar-left">
+            <h3>este es el lado derecho</h3>
+            <img src="css/img/chritsmats1.png" alt="Chritsmas-right">
+        </div>
+       <div class=" footer">
+            <form action="#">
+                <h1 >Mensaje Navideño</h1>
+                <input type="text" name="name" id="name" placeholder="Nombre"  class="inputText" required="">
+                <textarea name="comment" id="comment" cols="10" rows="10" class="inputText" placeholder="Mensaje navideño" required=""></textarea>
+               <!-- <input type="text" name="comment" id="comment" placeholder="Mensaje"  class="inputText" required="">-->
+                <input type="submit" value="Enviar" class="submitEnviar">
+            </form>
+        </div>
+        <div class="footer-left"></div>
+    </div>
 </body>
 </html>
-?>
