e170a084f985ed6c4ece52080b99483cde1b9123
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-01-28 14:43:58 -0600

    imagen

--- test.php
+++ test.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8">
         <title>Tests</title>
-        <link rel="stylesheet" href="../../css/style.css">
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <h1>Deploy de Php en Azure App Service</h1>
         <!-- Make a form for user data-->
         <form action="../../index.html" method="post">
             <input type="text" name="name" placeholder="Name">
