cf98b54bfe3111d9aa32d8264e8a6e9a59852a1d
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-01-28 14:54:54 -0600

    App service

--- index.php
+++ index.php
@@ -32,7 +32,17 @@ echo "
         </tbody>
     </table>
 ";
-<h1>Welcome to Tokio Japon</h1>
-<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.jrailpass.com%2Fblog%2Fes%2Flugares-y-tiendas-manga-anime-japon&psig=AOvVaw3uGl7UeFbuji0YmEQKbj7z&ust=1643488621760000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJCMn6-m1fUCFQAAAAAdAAAAABAD" alt="japon">
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome to Tokio Japon</h1>
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.jrailpass.com%2Fblog%2Fes%2Flugares-y-tiendas-manga-anime-japon&psig=AOvVaw3uGl7UeFbuji0YmEQKbj7z&ust=1643488621760000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJCMn6-m1fUCFQAAAAAdAAAAABAD" alt="japon">
+</body>
+</html>
 ?>
